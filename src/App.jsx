25b973df5f61227757766ca415af4ce3377d9324
Avinash <avinashkmg1310@gmail.com>
2024-10-01 11:54:58 +0530

    Initialize project using create vite@latest

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+// import React from 'react'
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import List from "./pages/List/List";
+import Single from "./pages/Single/Single";
+import New from "./pages/New/New";
+
+function App() {
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/">
+						<Route index element={<Home />} />
+						<Route path="login" element={<Login />} />
+						<Route path="users">
+							<Route index element={<List />} />
+							<Route path=":userId" element={<Single />} />
+							<Route path="new" element={<New />} />
+						</Route>
+						<Route path="products">
+							<Route index element={<List />} />
+							<Route path=":productId" element={<Single />} />
+							<Route path="new" element={<New />} />
+						</Route>
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
+}
+
+export default App;
